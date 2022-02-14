23226f25e8f163be926bb96a9d5e789e5bd9e9d9
pradityamf <pradityamf@gmail.com>
2022-02-14 13:54:29 +0700

    formix and yup validation.

--- src/pages/product/component/ProductForm.js
+++ src/pages/product/component/ProductForm.js
@@ -2,7 +2,9 @@ import { useEffect, useState } from "react"
 import { useLocation, useNavigate, useParams } from "react-router-dom"
 import { CircularProgress } from "@mui/material"
 import { createProduct, getProduct, updateProduct } from "../service/ProductService"
+import { useFormik } from 'formik'
 
+import * as Yup from 'yup'
 
 export const ProductFrom = () => {
   let params = useParams()
@@ -31,11 +33,32 @@ export const ProductFrom = () => {
     }
   }, [])
 
+  const formik = useFormik({
+    initialValues: {
+      id: "",
+      name: "",
+    },
+    validationSchema: Yup.object({
+      id : Yup.string().required("This field is required"),
+      name : Yup.string().required("This field is required").min(3, "Minimum 3 character"),
+    }),
+    onSubmit: () => {
+      if(params.id){
+        handleUpdate();
+      }else{
+        handleAdd()
+      }
+    }
+  })
+
   const getDataById = async () => {
     // const res = await axios.get(`http://localhost:3000/products/${params.id}`)
     const res = await getProduct(params.id)
     console.log("res data",res.data);
     setNewData(res.data)
+    formik.values.id=res.data.id
+    formik.values.name=res.data.name
+    formik.setFieldValue(res)
   }
 
   const handleOnChange = (event) => {
@@ -52,11 +75,13 @@ export const ProductFrom = () => {
     }
   }
 
-  const handleUpdate = async (data) => {
+  // const handleUpdate = async (data) => {
+  const handleUpdate = async () => {
       try {
         setLoading(true)
         // const res = await axios.put(`http://localhost:3000/products`,data)
-        const res = await updateProduct(data)
+        // const res = await updateProduct(data)
+        const res = await updateProduct(formik.values)
         console.log("res data", res.data);
         setLoading(false)
         navigate("..")
@@ -65,11 +90,13 @@ export const ProductFrom = () => {
       }
   }
 
-  const handleAdd = async (data) => {
+  // const handleAdd = async (data) => {
+  const handleAdd = async () => {
       try {
         setLoading(true)
         // const res = await axios.post(`http://localhost:3000/products`,data)
-        const res = await createProduct(data)
+        // const res = await createProduct(data)
+        const res = await createProduct(formik.values)
         console.log("res data", res.data);
         setLoading(false)
         navigate("..")
@@ -92,23 +119,52 @@ export const ProductFrom = () => {
         </>
         :
         <div>
+        <form onSubmit={formik.handleSubmit}>
         <h2>Product Form Update</h2>
             <div className="form-group row">
             <label htmlFor="inputId" className="col-sm-2 col-form-label">Id</label>
             <div className="col-sm-10">
               {/* handling kalo object ga ada isinya "useState({})", uncontrol form ke control form */}
-            <input value={newData.id || ''} type="text" className="form-control" id="inputId" placeholder="Id" name="id" onChange={handleOnChange}/>
+            {/* <input value={newData.id || ''} type="text" className="form-control" id="inputId" placeholder="Id" name="id" onChange={handleOnChange}/> */}
+            <input 
+            value={formik.values.id || ''} 
+            type="text" 
+            className="form-control" 
+            id="inputId" 
+            placeholder="Id" 
+            name="id" 
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            />
+            {formik.errors.id && formik.touched.id ? (
+            <small className="text-danger">{formik.errors.id}</small>
+            ) : null}
             </div>
         </div>
         <br></br>
         <div className="form-group row">
             <label htmlFor="inputName" className="col-sm-2 col-form-label">Name</label>
             <div className="col-sm-10">
-            <input value={newData.name || ''} type="text" className="form-control" id="inputName" placeholder="Name" name="name" onChange={handleOnChange}/>
+            {/* <input value={newData.name || ''} type="text" className="form-control" id="inputName" placeholder="Name" name="name" onChange={handleOnChange}/> */}
+            <input 
+            value={formik.values.name || ''} 
+            type="text" 
+            className="form-control" 
+            id="inputName" 
+            placeholder="Name" 
+            name="name" 
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            />
+            {formik.errors.name && formik.touched.name ? (
+            <small className="text-danger">{formik.errors.name}</small>
+            ) : null}
             </div>
         </div>
         <br></br>
-        <input className="btn btn-primary" type="submit" value="Submit" onClick={() => handleUpdate(newData)}/> 
+        {/* <input className="btn btn-primary" type="submit" value="Submit" onClick={() => handleUpdate(newData)}/>  */}
+        <input className="btn btn-primary" type="submit" value="Submit"/> 
+        </form>
         </div>
         :
         loading ?
@@ -120,22 +176,49 @@ export const ProductFrom = () => {
 
         <div>
         <h2>Product Form Add</h2>
-            <div className="form-group row">
+        <form onSubmit={formik.handleSubmit}>
+        <div className="form-group row">
             <label htmlFor="inputId" className="col-sm-2 col-form-label">Id</label>
             <div className="col-sm-10">
               {/* handling kalo object ga ada isinya "useState({})", uncontrol form ke control form */}
-            <input type="text" className="form-control" id="inputId" placeholder="Id" name="id" onChange={handleOnChange}/>
+            {/* <input type="text" className="form-control" id="inputId" placeholder="Id" name="id" onChange={handleOnChange}/> */}
+            <input 
+            type="text" 
+            className="form-control" 
+            id="inputId" 
+            placeholder="Id" 
+            name="id" 
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            />
+            {formik.errors.id && formik.touched.id ? (
+            <small className="text-danger">{formik.errors.id}</small>
+            ) : null}
             </div>
         </div>
         <br></br>
         <div className="form-group row">
             <label htmlFor="inputName" className="col-sm-2 col-form-label">Name</label>
             <div className="col-sm-10">
-            <input type="text" className="form-control" id="inputName" placeholder="Name" name="name" onChange={handleOnChange}/>
+            {/* <input type="text" className="form-control" id="inputName" placeholder="Name" name="name" onChange={handleOnChange}/> */}
+            <input 
+            type="text" 
+            className="form-control" 
+            id="inputName" 
+            placeholder="Name" 
+            name="name" 
+            onChange={formik.handleChange} 
+            onBlur={formik.handleBlur}
+            />
+            {formik.errors.name && formik.touched.name ? (
+            <small className="text-danger">{formik.errors.name}</small>
+            ) : null}
             </div>
         </div>
         <br></br>
-        <input className="btn btn-primary" type="submit" value="Submit" onClick={() => handleAdd(newData)}/> 
+        {/* <input className="btn btn-primary" type="submit" value="Submit" onClick={() => handleAdd(newData)}/>  */}
+        <input className="btn btn-primary" type="submit" value="Submit"/> 
+        </form>
         </div>
     }
     </div>
